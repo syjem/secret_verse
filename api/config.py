5045a03830f1f7be3835e6e15a14edf44b6fb4b0
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-07-15 14:18:03 +0800

    add flask-wtf csrf secret key

--- api/config.py
+++ api/config.py
@@ -8,4 +8,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     RAPID_API_KEY = os.getenv('RAPID_API_KEY')
+    WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
     DEBUG = False
